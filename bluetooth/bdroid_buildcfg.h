8387d109667e3254c5db2a2e2577a51e24c6102a
mhdzumair <mhdzumair@gmail.com>
2020-03-23 22:22:46 +0530

    make fresh woods device tree with ref from MAD
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,6 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME   "Moto E4"
+
+#endif
---
+++ device.mk
@@ -0,0 +1,81 @@
+LOCAL_PATH := device/motorola/woods
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Recovery allowed devices
+TARGET_OTA_ASSERT_DEVICE := woods,woods_f,Moto_E4,Moto E4,e4
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.mt6737m
+
+PRODUCT_PACKAGES += \
+    fingerprint.default
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml
+
+# Media
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# Keyboard layout
+PRODUCT_COPY_FILES += \
+  $(LOCAL_PATH)/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
+  $(LOCAL_PATH)/configs/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
+  $(LOCAL_PATH)/configs/AVRCP.kl:system/usr/keylayout/AVRCP.kl
+
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
+		$(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
+		$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+		$(LOCAL_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
+		$(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc
+
+# TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/.ht120.mtc:system/etc/.tp/.ht120.mtc \
+    $(LOCAL_PATH)/configs/thermal/thermal.conf:system/etc/.tp/thermal.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal.off.conf:system/etc/.tp/thermal.off.conf \
+    $(LOCAL_PATH)/configs/thermal/.thermal_policy_00:system/etc/.tp/.thermal_policy_00
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+# Dalvik/HWUI
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Common stuff
+$(call inherit-product, vendor/mad/config/common.mk)
+
+#TODO
+# Vendor
+$(call inherit-product, vendor/motorola/woods/note3-vendor.mk)
+$(call inherit-product, vendor/coolpad/note3/note3lite-firmware-blobs.mk)
