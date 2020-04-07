7e807a70a36ba5678627c7c969b2e693112878fb
mhdzumair <mhdzumair@gmail.com>
2020-04-07 07:20:44 +0530

    device:removed file name already deleted

--- device.mk
+++ device.mk
@@ -60,12 +60,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-		$(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-		$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
-		$(LOCAL_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
 		$(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
     $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
     $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
