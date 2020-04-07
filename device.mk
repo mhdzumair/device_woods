11792d4e976d84f8712cf64749cfc227c6260045
mhdzumair <mhdzumair@gmail.com>
2020-04-07 07:19:23 +0530

    device:removed file name already deleted

--- device.mk
+++ device.mk
@@ -60,8 +60,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-		$(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-		$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
