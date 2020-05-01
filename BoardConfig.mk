3389dd25e74cec949de86493395f751611905f4e
mhdzumair <mhdzumair@gmail.com>
2020-05-01 15:55:09 +0530

    dt: remove unwanted stuff & make it make it omni twrp tree
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,25 @@
-# mt6737m platform boardconfig
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Copyright 2020 Mohamed Zumair <mhdzumair@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := device/motorola/woods
 
 # Platform
-ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOARD_PLATFORM := mt6737m
-TARGET_NO_BOOTLOADER := true
 TARGET_NO_FACTORYIMAGE := true
 
 # CPU
@@ -13,9 +28,12 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
 #Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := mt6737m
+TARGET_BOOTLOADER_BOARD_NAME := MT6735
+TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
@@ -24,93 +42,73 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x04000000
 BOARD_TAGS_OFFSET := 0x0e000000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive androidboot.selinux=disabled
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
 TARGET_KERNEL_ARCH := arm
 MTK_APPENDED_DTB_SUPPORT := yes
-TARGET_KMODULES := true
+TARGET_KERNEL_SOURCE := kernel/motorola/woods
+TARGET_KERNEL_CONFIG := woods_defconfig
 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/zImage-dtb
-PRODUCT_COPY_FILES += \
-    $(TARGET_PREBUILT_KERNEL):kernel
+#toolchain
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/gcc-linaro-7.5/bin
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 11419516928
-BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
 BOARD_FLASH_BLOCK_SIZE := 131072
-
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 
-# Use custom init.rc
-TARGET_PROVIDES_INIT_RC := true
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Display
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Moto_E4,Moto E4,E4,e4,woods,woods_f,woods_retail
 
 # Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery.fstab
 
-# Recovery
-RECOVERY_VARIANT := twrp
-
-# TWRP
-ifeq ($(RECOVERY_VARIANT), twrp)
+#TWRP stuff
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_THEME := portrait_hdpi
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_MAX_BRIGHTNESS := 255
 BOARD_SUPPRESS_SECURE_ERASE := true
+TW_INCLUDE_FB2PNG := true
 TW_INCLUDE_CRYPTO := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_BRIGHTNESS:= 100
 TW_MAX_BRIGHTNESS := 255
 TW_NO_USB_STORAGE := true
 BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
 TARGET_DISABLE_TRIPLE_BUFFERING := false
 TW_USE_TOOLBOX := true
-else
-# CWM
-BOARD_RECOVERY_SWIPE := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-endif
-
-# TWRP-specific
-ifeq ($(RECOVERY_VARIANT), twrp)
-DEVICE_RESOLUTION := 720x1280
-DEVICE_SCREEN_WIDTH := 720
-DEVICE_SCREEN_HEIGHT := 1280
+TW_REBOOT_RECOVERY := true
 RECOVERY_SDCARD_ON_DATA := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-endif
-
+TW_HAS_MTP := true
+TW_MTP_DEVICE := /dev/mtp_usb
 TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TW_DEFAULT_LANGUAGE := en
 TW_EXTRA_LANGUAGES := true
 
-# exFAT FS Support
-TW_INCLUDE_FUSE_EXFAT := true
 
-# NTFS Support
-TW_INCLUDE_FUSE_NTFS := true
-
-
-TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote32
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_USES_MTK_HARDWARE := true
+MTK_HARDWARE := true
