e5c451ba67028140bb7e4d2f44c13aa8c48c221c
mhdzumair <mhdzumair@gmail.com>
2020-08-29 18:02:07 +0000

    device:lazy commit

--- device.mk
+++ device.mk
@@ -76,19 +76,19 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-		$(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:root/factory_init.project.connectivity.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:root/factory_init.project.connectivity.rc \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
-		$(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-		$(LOCAL_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-		$(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-		$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
+    $(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
-		$(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
-		$(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-		$(LOCAL_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
-		$(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
+    $(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc
 
 # TWRP
